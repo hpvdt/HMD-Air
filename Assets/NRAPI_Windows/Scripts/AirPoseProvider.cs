df391783c8a77af22e63305a0b676179659d5eee
Peng Cheng <peng.cheng@acyclic.ai>
2023-07-06 20:34:37 -0400

    Fix the bug in AirAPI updating
    TryConnect & TryConnect both change connectionState, which is now an enum
    
    upgrade AirAPI
    
    add a connect button

--- Assets/NRAPI_Windows/Scripts/AirPoseProvider.cs
+++ Assets/NRAPI_Windows/Scripts/AirPoseProvider.cs
@@ -8,8 +8,48 @@ public class AirPoseProvider : BasePoseProvider
     [DllImport("AirAPI_Windows", CallingConvention = CallingConvention.Cdecl)]
     public static extern int StartConnection();
 
+    protected enum ConnectionStates
+    {
+        Disconnected = 0,
+        Offline = 1,
+        StandBy = 2,
+
+        Connected = 3
+    }
+
+    protected static ConnectionStates connectionState = ConnectionStates.Disconnected;
+
+    public bool IsConnecting()
+    {
+        return connectionState is ConnectionStates.Connected or ConnectionStates.StandBy;
+    }
+
+    public void TryConnect()
+    {
+        // Start the connection
+        var code = StartConnection();
+        if (code == 1)
+            connectionState = ConnectionStates.StandBy;
+        else
+            Debug.Log("Connection error: return code " + code);
+    }
+
     [DllImport("AirAPI_Windows", CallingConvention = CallingConvention.Cdecl)]
     public static extern int StopConnection();
+
+    public void TryDisconnect()
+    {
+        var code = StopConnection();
+        if (code == 1)
+        {
+            connectionState = ConnectionStates.Disconnected;
+        }
+        else
+        {
+            connectionState = ConnectionStates.Offline;
+            Debug.LogError("Disconnection error: return code " + code);
+        }
+    }
 
     [DllImport("AirAPI_Windows", CallingConvention = CallingConvention.Cdecl)]
     public static extern IntPtr GetQuaternion();
@@ -19,9 +59,9 @@ public class AirPoseProvider : BasePoseProvider
 
     public float mouseSensitivity = 100.0f;
 
-    protected int ConnectionStatus = 0;
-
     protected Quaternion FromGlasses = Quaternion.identity;
+
+    protected static Quaternion NO_READING_Q = Quaternion.Euler(90f, 0, 0);
 
     protected Vector2 FromMouseXY = Vector2.zero;
 
@@ -30,60 +70,59 @@ public class AirPoseProvider : BasePoseProvider
     // Start is called before the first frame update
     private void Start()
     {
-        // Start the connection
-        ConnectionStatus = StartConnection();
+        TryConnect();
+    }
+
+    private void OnDestroy()
+    {
+        TryDisconnect();
     }
 
     // Update Pose
     public override bool TryGetPoseFromProvider(out Pose output)
     {
-        // Debug.Log("status is" + ConnectionStatus);
+        if (IsConnecting()) UpdateFromGlasses();
 
-        if (ConnectionStatus == 0)
-        {
-            var ptr = GetEuler();
-            var arr = new float[3];
-            Marshal.Copy(ptr, arr, 0, 3);
-
-            FromGlasses = Quaternion.Euler(-arr[1] + 90.0f, -arr[2], -arr[0]);
-        }
-
-        if (Input.GetMouseButton(1))
-        {
-            var deltaX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-            var deltaY = -Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-
-            // Debug.Log("mouse pressed" + mouseX + ";" + mouseY);
-
-            FromMouseXY += new Vector2(deltaX, deltaY);
-
-            FromMouse = Quaternion.Euler(-FromMouseXY[1], -FromMouseXY[0], 0.0f);
-        }
+        if (Input.GetMouseButton(1)) UpdateFromMouse();
 
         var compound = FromGlasses * FromMouse;
 
-        // Quaternion target = Quaternion.Euler(arr[1], -arr[2], -arr[0]);
-        // Quaternion target = Quaternion.Euler(45, 0, 0);
         output = new Pose(new Vector3(0, 0, 0), compound);
         return true;
     }
 
-    // void Update()
-    // {
-    //     // if (Input.GetMouseButtonDown(1))
-    //     // {
-    //     // }
-    // }
+    private void UpdateFromGlasses()
+    {
+        var ptr = GetEuler();
+        var arr = new float[3];
+        Marshal.Copy(ptr, arr, 0, 3);
 
-    // Use quaternion directly
-    // public override bool TryGetPoseFromProvider(out Pose output)
-    // {
-    //     IntPtr ptr = GetQuaternion();
-    //     float[] arr = new float[4];
-    //     Marshal.Copy(ptr, arr, 0, 4);
-    //
-    //     Quaternion target = Quaternion(arr[1], arr[2], -arr[0] + 180.0f);
-    //     output = new Pose(new Vector3(0, 0, 0), target);
-    //     return true;
-    // }
+        // Debug.Log("glasses input (Euler): " + arr);
+
+        var reading = Quaternion.Euler(-arr[1] + 90.0f, -arr[2], -arr[0]);
+        if (connectionState == ConnectionStates.StandBy)
+        {
+            if (!reading.Equals(NO_READING_Q))
+            {
+                connectionState = ConnectionStates.Connected;
+                FromGlasses = reading;
+            }
+        }
+        else
+        {
+            FromGlasses = reading;
+        }
+    }
+
+    private void UpdateFromMouse()
+    {
+        var deltaX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        var deltaY = -Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        FromMouseXY += new Vector2(deltaX, deltaY);
+
+        // Debug.Log("mouse pressed:" + FromMouseXY);
+
+        FromMouse = Quaternion.Euler(-FromMouseXY[1], -FromMouseXY[0], 0.0f);
+    }
 }
