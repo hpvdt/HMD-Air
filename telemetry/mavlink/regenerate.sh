31860a21dcd4ae2c4cbd0141779c383543805fd2
peng <peng.cheng@acyclic.ai>
2023-12-30 17:36:20 -0500

    regenerate with revised target dir

--- telemetry/mavlink/regenerate.sh
+++ telemetry/mavlink/regenerate.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-python -m pymavlink.tools.mavgen --lang=CS --wire-protocol=2.0 --output="__generated/cs" "message_definitions/ardupilotmega.xml" "message_definitions/offspec.xml"
+python -m pymavlink.tools.mavgen --lang=CS --wire-protocol=2.0 --output="__generated/csharp" "message_definitions/ardupilotmega.xml" "message_definitions/offspec.xml"
 
-#python -m pymavlink.tools.mavgen --lang=WLua --wire-protocol=2.0 "message_definitions/ardupilotmega.xml" "message_definitions/offspec.xml"
+python -m pymavlink.tools.mavgen --lang=Python --wire-protocol=2.0 --output="__generated/python" "message_definitions/ardupilotmega.xml" "message_definitions/offspec.xml"
+
+#python -m pymavlink.tools.mavgen --lang=C --wire-protocol=2.0 --output="__generated/c" "message_definitions/ardupilotmega.xml" "message_definitions/offspec.xml"
