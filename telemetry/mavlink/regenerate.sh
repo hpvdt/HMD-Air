4db883597adf5cfe6c44cf65f2fb01c68f7a90cf
Peng Cheng <peng.cheng@acyclic.ai>
2024-01-06 16:49:00 -0500

    remove regenerate.bat which is obsolete
    cd into pythonpath in regenerate.sh

--- telemetry/mavlink/regenerate.sh
+++ telemetry/mavlink/regenerate.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+CRDIR="$(
+  cd "$(dirname "$0")" || exit
+  pwd
+)"
+
+cd "$CRDIR" || exit
+
 python -m pymavlink.tools.mavgen --lang=CS --wire-protocol=2.0 --output="__generated/csharp" "message_definitions/ardupilotmega.xml" "message_definitions/offspec.xml"
 
 python -m pymavlink.tools.mavgen --lang=Python --wire-protocol=2.0 --output="__generated/python" "message_definitions/ardupilotmega.xml" "message_definitions/offspec.xml"
