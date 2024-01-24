e9b25b23c8de5f5cb880e6ad68bb07cb83f76cf1
peng <peng.cheng@acyclic.ai>
2024-01-23 21:00:58 -0500

    disable C and CSharp generation

--- telemetry/mavlink/regenerate.sh
+++ telemetry/mavlink/regenerate.sh
@@ -7,10 +7,8 @@ CRDIR="$(
 
 cd "$CRDIR" || exit
 
-python -m pymavlink.tools.mavgen --lang=CS --wire-protocol=2.0 --output="__generated/csharp" "message_definitions/common.xml"
-
 python -m pymavlink.tools.mavgen --lang=Python --wire-protocol=2.0 --output="__generated/python" "message_definitions/common.xml"
 
-python -m pymavlink.tools.mavgen --lang=C --wire-protocol=2.0 --output="__generated/C" "message_definitions/common.xml"
+#python -m pymavlink.tools.mavgen --lang=CSharp --wire-protocol=2.0 --output="__generated/csharp" "message_definitions/common.xml"
 
-#python -m pymavlink.tools.mavgen --lang=C --wire-protocol=2.0 --output="__generated/c" "message_definitions/ardupilotmega.xml" "message_definitions/offspec.xml"
+#python -m pymavlink.tools.mavgen --lang=C --wire-protocol=2.0 --output="__generated/C" "message_definitions/common.xml"
---
+++ telemetry/mavlink/test/LitmusTest.py
@@ -0,0 +1,4 @@
+import __generated.python as mavlink
+
+if __name__ == '__main__':
+    example1 = mavlink.attitude_quaternion_encode()
