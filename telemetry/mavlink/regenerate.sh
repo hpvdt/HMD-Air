4c728669afb6d33c0dff5e716bf3ec9bf0ac6870
peng <peng.cheng@acyclic.ai>
2024-01-23 21:16:00 -0500

    add mkdir

--- telemetry/mavlink/regenerate.sh
+++ telemetry/mavlink/regenerate.sh
@@ -7,6 +7,8 @@ CRDIR="$(
 
 cd "$CRDIR" || exit
 
+mkdir __generated
+
 python -m pymavlink.tools.mavgen --lang=Python --wire-protocol=2.0 --output="__generated/python" "message_definitions/common.xml"
 
 #python -m pymavlink.tools.mavgen --lang=CSharp --wire-protocol=2.0 --output="__generated/csharp" "message_definitions/common.xml"
