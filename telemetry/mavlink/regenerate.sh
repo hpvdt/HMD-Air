c86ebc51ef354857ce5a2e14e18d0fb464816c30
peng <peng.cheng@acyclic.ai>
2024-01-23 20:03:17 -0500

    reduce to common, only python is kept

--- telemetry/mavlink/regenerate.sh
+++ telemetry/mavlink/regenerate.sh
@@ -7,8 +7,10 @@ CRDIR="$(
 
 cd "$CRDIR" || exit
 
-python -m pymavlink.tools.mavgen --lang=CS --wire-protocol=2.0 --output="__generated/csharp" "message_definitions/ardupilotmega.xml"
+python -m pymavlink.tools.mavgen --lang=CS --wire-protocol=2.0 --output="__generated/csharp" "message_definitions/common.xml"
 
-python -m pymavlink.tools.mavgen --lang=Python --wire-protocol=2.0 --output="__generated/python" "message_definitions/ardupilotmega.xml"
+python -m pymavlink.tools.mavgen --lang=Python --wire-protocol=2.0 --output="__generated/python" "message_definitions/common.xml"
+
+python -m pymavlink.tools.mavgen --lang=C --wire-protocol=2.0 --output="__generated/C" "message_definitions/common.xml"
 
 #python -m pymavlink.tools.mavgen --lang=C --wire-protocol=2.0 --output="__generated/c" "message_definitions/ardupilotmega.xml" "message_definitions/offspec.xml"
