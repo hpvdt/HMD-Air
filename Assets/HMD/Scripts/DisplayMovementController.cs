c80a6dce43643ade4ff51ba8083674dd543a749f
AAAndrewEEE <evanyshyna@gmail.com>
2024-03-30 18:18:43 -0400

    Completed new compass halo
    More work on heading halo
    
    Added new compass halo
    
    modified datadisplay prefab
    
    added HUD rotation
    
    Added libar_drivers.dylib and modified AirPoseProvider to be compatible with macos
    
    deleted more blend files
    
    keyboard controller
    
    Fixed binary decoder
    
    more keyboard control changes
    
    Completed Keyboard controller Rotation
    
    Update projectversion
    
    more work on wind direction halo (not working)
    
    Added wind direction material
    
    minor changes
    
    SerialReader connected to vision overlay
    
    Serial port working again. SerialReader is now integrated into the vision overlay. BinaryDecoder is now obsolete.
    
    Removed blend files
    
    Removed blend files
    
    Completed Keyboard controller Rotation
    
    more keyboard control changes
    
    Fixed binary decoder
    
    keyboard controller
    
    minor changes

---
+++ Assets/HMD/Scripts/DisplayMovementController.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayMovementController : MonoBehaviour
+{
+
+    public bool keyboardController = false;
+    public float rotationSpeed = 10f;
+    public float movementSpeed = 10f;
+    public Transform halo;
+    public Transform camera;
+
+    public Transform HUDDisplay;
+
+    Vector3 rotationVector = new Vector3(0, 0, 0);
+
+    void Update()
+    {
+        float movementAmount = movementSpeed * Time.deltaTime;
+
+
+        if (keyboardController)
+        {
+
+
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                BinaryDecoder.GPSY += movementAmount;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                BinaryDecoder.GPSX += -movementAmount;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                BinaryDecoder.GPSY += -movementAmount;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                BinaryDecoder.GPSX += movementAmount;
+            }
+            if (Input.GetKey(KeyCode.Space))
+            {
+                BinaryDecoder.Altimeter += movementAmount;
+            }
+            if (Input.GetKey(KeyCode.C))
+            {
+                BinaryDecoder.Altimeter += -movementAmount;
+            }
+
+            // Handle rotation inputs
+            float rotationAmount = rotationSpeed * Time.deltaTime;
+
+            if (Input.GetKey(KeyCode.K))
+            {
+                // Rotate up
+                //halo.Rotate(Vector3.right, rotationAmount);
+                rotationVector += Vector3.right * rotationAmount;
+            }
+
+            if (Input.GetKey(KeyCode.I))
+            {
+                // Rotate down
+                //halo.Rotate(Vector3.left, rotationAmount);
+                rotationVector += Vector3.left * rotationAmount;
+
+            }
+
+            if (Input.GetKey(KeyCode.U))
+            {
+                // Rotate cork screw left
+                //halo.Rotate(Vector3.forward, rotationAmount);
+                rotationVector += Vector3.forward * rotationAmount;
+
+            }
+
+            if (Input.GetKey(KeyCode.O))
+            {
+                // Rotate cork screw right
+                //halo.Rotate(Vector3.back, rotationAmount);
+                rotationVector += Vector3.back * rotationAmount;
+
+            }
+
+            if (Input.GetKey(KeyCode.J))
+            {
+                BinaryDecoder.heading += rotationAmount;
+            }
+            if (Input.GetKey(KeyCode.L))
+            {
+                BinaryDecoder.heading -= rotationAmount;
+            }
+            if (BinaryDecoder.heading < 0){
+                BinaryDecoder.heading +=360;
+            }
+            else if (BinaryDecoder.heading > 360){
+                BinaryDecoder.heading -= 360;
+            }
+
+            Quaternion rotation = Quaternion.Euler(rotationVector);
+            //Debug.Log("controller: " + rotation.x + " " + rotation.y + " " + rotation.z + " " + rotation.w);
+
+            BinaryDecoder.IMU.x = rotation.x;
+            BinaryDecoder.IMU.y = rotation.y;
+            BinaryDecoder.IMU.z = rotation.z;
+            BinaryDecoder.IMU.w = rotation.w;
+        }
+
+        Vector3 target = new Vector3(BinaryDecoder.GPSX, BinaryDecoder.Altimeter, BinaryDecoder.GPSY);
+
+        //Debug.Log("target: " + BinaryDecoder.GPSX + " " + BinaryDecoder.Altimeter + " " + BinaryDecoder.GPSY + "\n");
+        //Debug.Log("transform.position: " + transform.position.x + " " + transform.position.y + " " + transform.position.z);
+
+        transform.position = Vector3.MoveTowards(transform.position, target, movementAmount);
+        camera.position = Vector3.MoveTowards(camera.position, target, movementAmount);
+
+        halo.rotation = Quaternion.Inverse(BinaryDecoder.IMU) * Quaternion.Euler(0, -BinaryDecoder.heading, 0);
+        //HUDDisplay.rotation = Quaternion.Inverse(BinaryDecoder.IMU);
+        Vector3 vector = new Vector3(0, -BinaryDecoder.heading, 0);
+        //halo.Rotate(0, -BinaryDecoder.heading, 0);
+        HUDDisplay.rotation = Quaternion.Euler(0,BinaryDecoder.heading,0);
+
+        
+
+    }
+}
